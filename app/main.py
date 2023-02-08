fc9bad59cde463f7dc575af83095d069040c45a4
antooni <antoni.pawlak@protonmail.com>
2023-02-08 23:15:06 +0100

    resovle errors

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ import sys
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
-    completed_process = subprocess.run([command, *args], capture_output=True, stdout=PIPE, stderr=PIPE, text=True)
+    completed_process = subprocess.run([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    completed_process.wait()
+
     print(completed_process.stdout)
     print(completed_process.stderr)
 
