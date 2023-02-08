3c37c0ec33335eea9513dc25acd3394986769600
antooni <antoni.pawlak@protonmail.com>
2023-02-08 23:33:09 +0100

    change to capture_output

--- app/main.py
+++ app/main.py
@@ -5,12 +5,12 @@ import sys
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
-    completed_process = subprocess.run([command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    completed_process = subprocess.run([command, *args], capture_output=True)
     
     completed_process.wait()
 
-    print(completed_process.stderr)
-    print(completed_process.stdout)
+    print(completed_process.stderr.decode())
+    print(completed_process.stdout.decode())
 
 
 if __name__ == "__main__":
