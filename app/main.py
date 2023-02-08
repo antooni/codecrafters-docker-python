188894f687f5e885b6349dffd417b7cb3caed6c6
antooni <antoni.pawlak@protonmail.com>
2023-02-08 23:11:31 +0100

    text=True

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import sys
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
-    completed_process = subprocess.run([command, *args], capture_output=True, stdout=PIPE, stderr=PIPE)
+    completed_process = subprocess.run([command, *args], capture_output=True, stdout=PIPE, stderr=PIPE, text=True)
     print(completed_process.stdout)
     print(completed_process.stderr)
 
