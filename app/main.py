05c06c8bea1a78c98bfbca0afbaffd0ead2f3ba0
antooni <antoni.pawlak@protonmail.com>
2023-02-08 22:55:18 +0100

    1 stage

--- app/main.py
+++ app/main.py
@@ -8,11 +8,11 @@ def main():
 
     # Uncomment this block to pass the first stage
     #
-    # command = sys.argv[3]
-    # args = sys.argv[4:]
+    command = sys.argv[3]
+    args = sys.argv[4:]
     #
-    # completed_process = subprocess.run([command, *args], capture_output=True)
-    # print(completed_process.stdout.decode("utf-8"))
+    completed_process = subprocess.run([command, *args], capture_output=True)
+    print(completed_process.stdout.decode("utf-8"))
 
 
 if __name__ == "__main__":
