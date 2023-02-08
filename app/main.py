b1dfbc4cddfd476170fc4640febdc5c4c5ec7248
antooni <antoni.pawlak@protonmail.com>
2023-02-08 23:07:26 +0100

    2 stage

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     #
-    completed_process = subprocess.run([command, *args], capture_output=True)
+    completed_process = subprocess.run([command, *args], capture_output=True, stdout=PIPE, stderr=PIPE)
     print(completed_process.stdout.decode("utf-8"))
 
 
