8e88c5f7731496fa91e13603a1a999a2e6c154f0
antooni <antoni.pawlak@protonmail.com>
2023-02-19 14:26:25 +0100

    chroot

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 import subprocess
 import sys
+import os
+
+PATH = "/tmp/codecrafters/docker"
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
+
+    os.mkdir(PATH)
+    os.chroot(PATH)
+
     completed_process = subprocess.run([command, *args], capture_output=True)
     
     sys.stdout.buffer.write(completed_process.stdout)
