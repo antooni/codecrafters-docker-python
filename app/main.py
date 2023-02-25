c0e8af31fa3571d4c729e16cc77b362dd9caaadd
antooni <antoni.pawlak@protonmail.com>
2023-02-25 14:52:44 +0100

    use ctypes

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import os
 import shutil
+import ctypes
 
 PATH = "/tmp/codecrafters/docker"
 
@@ -21,8 +22,12 @@ def main():
     shutil.copy('/usr/bin/unshare', os.path.join(PATH, 'usr/bin/unshare'))
 
     os.chroot(PATH)
+    libc = ctypes.CDLL(None)
+    CLONE_NEWPID = 0x20000000
+    libc.unshare.argtypes = [ctypes.c_int]
+    libc.unshare(CLONE_NEWPID)
 
-    completed_process = subprocess.run(['unshare', '--pid', command, *args], capture_output=True)
+    completed_process = subprocess.run([command, *args], capture_output=True)
     
     sys.stdout.buffer.write(completed_process.stdout)
     sys.stderr.buffer.write(completed_process.stderr)
